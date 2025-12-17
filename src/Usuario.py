5e36310d33cba75e541c6f00ee70386633ec5f36
Weber Fernandes <44792014+weberfern@users.noreply.github.com>
2025-12-17 23:07:03 +0000

    Corrigido arquivo Usuario.py e criado main.py

---
+++ src/Usuario.py
@@ -0,0 +1,21 @@
+class Usuario:
+    def __init__ (self, nome, email, id, historico_tentativas):
+        self.nome = nome
+        self.email = email
+        self.id = id
+        self.historico_tentativas = historico_tentativas
+
+    def salvar (self):
+        pass
+
+    def mostrar (self):
+        pass
+    
+    def atualizar (self):
+        pass
+    
+    def deletar (self):
+        pass
+    
+    def tentar_quiz (self):
+        pass
