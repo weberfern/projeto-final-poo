4270e8b0a4ca81489e138bb880b167e0fcdfb9a7
weberfern <weber12@gmail.com>
2025-12-17 20:28:49 -0300

    Atualizado classe Tentativa (inserido getters e setters

--- src/Tentativa.py
+++ src/Tentativa.py
@@ -10,6 +10,50 @@ class Tentativa:
     @property
     def quiz_referencia(self):
         return self.__quiz_referencia
+    
+    @quiz_referencia.setter
+    def quiz_referencia(self, quiz_referencia):
+        self.__quiz_referencia = quiz_referencia
+
+    @property
+    def usuario_referencia(self):
+        return self.__usuario_referencia
+    
+    @usuario_referencia.setter
+    def usuario_referencia(self, usuario_referencia):
+        self.__usuario_referencia = usuario_referencia
+
+    @property
+    def respostas_dadas(self):
+        return self.__respostas_dadas
+    
+    @respostas_dadas.setter
+    def respostas_dadas(self, respostas_dadas):
+        self.__respostas_dadas = respostas_dadas
+
+    @property
+    def pontuacao_obtida(self):
+        return self.__pontuacao_obtida
+    
+    @pontuacao_obtida.setter
+    def pontuacao_obtida(self, pontuacao_obtida):
+        self.__pontuacao_obtida = pontuacao_obtida
+
+    @property
+    def taxa_acerto(self):
+        return self.__taxa_acerto
+    
+    @taxa_acerto.setter
+    def taxa_acerto(self, taxa_acerto):
+        self.__taxa_acerto = taxa_acerto
+
+    @property
+    def tempo_total(self):
+        return self.__tempo_total
+    
+    @tempo_total.setter
+    def tempo_total(self, tempo_total):
+        self.__tempo_total = tempo_total
 
     def salvar(self):
         pass
