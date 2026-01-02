582f57bf1f15ee17e71cbb3f85e2c9dd71c8ebda
marcelosantosalves <marcelocaririceara@gmail.com>
2026-01-02 15:34:26 -0300

    Atualizar o Quiz.py

---
+++ src/Quiz.py
@@ -0,0 +1,69 @@
+class Quiz:
+    
+    def __init__(self, titulo: str, perguntas: list, pontos_max: int, num_tentativas: int, tempo_limite: int):
+        self.titulo = titulo 
+        self.perguntas = perguntas
+        self.pontos_max = pontos_max
+        self.num_tentativas = num_tentativas
+        self.tempo_limite = tempo_limite
+
+    ########################## GETTERS E SETTERS DE CADA ATRIBUTO DA CLASSE ##############################
+    
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, value):
+        self.__titulo = value
+    
+    @property
+    def perguntas(self):
+        return self.__perguntas
+
+    @perguntas.setter
+    def perguntas(self, value):
+        self.__perguntas = value
+    
+    @property
+    def pontos_max(self):
+        return self.__pontos_max
+
+    @pontos_max.setter
+    def pontos_max(self, value):
+        self.__pontos_max = value
+    
+    @property
+    def num_tentativas(self):
+        return self.__num_tentativas
+
+    @num_tentativas.setter
+    def num_tentativas(self, value):
+        self.__num_tentativas = value
+    
+    @property
+    def tempo_limite(self):
+        return self.__tempo_limite
+
+    @tempo_limite.setter
+    def tempo_limite(self, value):
+        self.__tempo_limite = value
+
+    ################################### MÉTODOS DA CLASSE##################################
+    
+
+    def iniciar_quiz(self):
+        pass 
+
+    def exibir_perguntas(self):
+        pass 
+
+    def calcular_pontuacao_max(self):
+        pass 
+
+    def verificar_tentativas(self):
+        pass 
+
+    def verificar_tempo_limite(self):
+        pass
