268d93fed37bdfb5a6c355ea896ed8e813c5e62a
Jefferson <rodriguesjefferson779@gmail.com>
2026-01-07 04:39:21 -0300

    Implementa classes Pergunta e PerguntaMultiplaEscolha

---
+++ src/Pergunta.py
@@ -0,0 +1,46 @@
+class Pergunta:
+    def __init__(self, cod_pergunta, enunciado, dificuldade, tema):
+        self.cod_pergunta = cod_pergunta
+        self.enunciado = enunciado
+        self.dificuldade = dificuldade
+        self.tema = tema
+        
+    @property
+    def dificuldade(self):
+        return self._dificuldade
+
+    @dificuldade.setter
+    def dificuldade(self, valor):
+        if valor not in ["Fácil", "Médio", "Difícil"]:
+            raise ValueError("Dificuldade inválida.")
+        self._dificuldade = valor
+
+    def mostrar(self):
+        print(f"[{self.cod_pergunta}] {self.enunciado} ({self.dificuldade} • {self.tema})")
+
+    def obter_dificuldade(self):
+        return self.dificuldade
+
+    def salvar(self):
+        print(f"Pergunta {self.cod_pergunta} salva.")
+
+    def atualizar(self, novo_enunciado=None, novo_tema=None, nova_dificuldade=None):
+        if novo_enunciado:
+            self.enunciado = novo_enunciado
+        if novo_tema:
+            self.tema = novo_tema
+        if nova_dificuldade:
+            self.dificuldade = nova_dificuldade
+        print(f"Pergunta {self.cod_pergunta} atualizada.")
+
+
+    def deletar(self):
+        print(f"Pergunta {self.cod_pergunta} deletada.")
+
+    def verificar_resposta(self, resposta_usuario):
+        raise NotImplementedError()
+
+    def mostrar_resposta(self):
+        raise NotImplementedError()
+
+        
---
+++ src/PerguntaMultiplaEscolha.py
@@ -0,0 +1,16 @@
+from src.Pergunta import Pergunta
+
+class PerguntaMultiplaEscolha(Pergunta):
+    
+    def __init__(self, cod_pergunta, enunciado, dificuldade, tema, alternativas, resposta_indice):
+        
+        super().__init__(cod_pergunta, enunciado, dificuldade, tema)
+        
+        self.alternativas = alternativas
+        self.resposta_indice = resposta_indice
+        
+    def verificar_resposta(self, resposta_usuario):
+        if resposta_usuario == self.resposta_indice:
+            return True
+        else:
+            return False
