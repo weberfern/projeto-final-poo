1e3b36187cfa06f0fecf6d63e7f0008ec421f6d9
Weber Fernandes <44792014+weberfern@users.noreply.github.com>
2026-01-08 01:21:03 +0000

    Exportado classe PerguntaMultiplaEscolha no __init__.py e importado no main.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from .Usuario import Usuario
 from .Tentativa import Tentativa
 from .Pergunta import Pergunta
+from .PerguntaMultiplaEscolha import PerguntaMultiplaEscolha
 from .Quiz import Quiz
 
-__all__ = ['Usuario', 'Tentativa', 'Pergunta', 'Quiz']
+__all__ = ['Usuario', 'Tentativa', 'Pergunta', 'PerguntaMultiplaEscolha', 'Quiz']
